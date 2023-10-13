2b3896d8d3d006620e66485bec269406566d9a2f
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-12 21:38:23 -0500

    working on fetch functions

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,16 +8,19 @@ fetch(queryURL)
   })
   .then(function (data) {
     // here you can take the data you receive and send any part of it to the next then() block.
-    const coolData = data.stuff
-    return coolData
+    const latitude = data.lat;
+    const long = data.lon;
+    return latitude
+    return long
   })
   .then(function (coolDataFromAbove) {
-    // use the coolDataFromAbove for the next fetch call
-    fetch("url-from-cooldata")
+    // use the latitude and longitude for the next fetch call
+    fetch("api.openweathermap.org/data/2.5/forecast?lat=latitude&lon=long&appid=6ff33c0653b3c13e330cc0ca224708a4")
       .then(function (response) {
         return response.json()
       })
       .then(function (finalData) {
         // Here is your final data!!
+        const weather = weather.main;
       })
   });
