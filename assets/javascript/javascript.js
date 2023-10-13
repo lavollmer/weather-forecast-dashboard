31c22efc774f581da4f2117f8b4ed813457009d6
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-12 22:46:42 -0500

    styling with bootstrap

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,7 +2,7 @@ var APIKey = '6ff33c0653b3c13e330cc0ca224708a4';
 var city = 'London';
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch(queryURL)
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=6ff33c0653b3c13e330cc0ca224708a4")
   .then(function (response) {
     return response.json()
   })
