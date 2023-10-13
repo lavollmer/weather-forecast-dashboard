25c88c76c8b8fdb0bcc33a8eac20575ca57539c8
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-13 09:21:18 -0500

    working on js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,7 @@
 const APIKey = '02198656acbfe2c2afe6c0d052bc513c';
 var city = 'London';
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var queryURLtwo = "api.openweathermap.org / data / 2.5 / forecast ? lat = ${lat} & lon=${lon} & appid=" + APIkey;
+var queryURLtwo = "api.openweathermap.org / data / 2.5 / forecast ? lat = ${lat} & lon=${lon} & appid=" + APIKey;
 
 fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&appid=02198656acbfe2c2afe6c0d052bc513c")
   .then(function (response) {
@@ -9,19 +9,23 @@ fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&appid=02198656acbf
   })
   .then(function (data) {
     // here you can take the data you receive and send any part of it to the next then() block.
-    var lat = data.lat;
-    var long = data.lon;
-    return { lat, long } = moreData;
+    var latitude = data[0].lat;
+    var longitude = data[0].lon;
+    console.log(latitude);
+    console.log(longitude);
+    var moreData = { lat: latitude, lon: longitude };
+    return moreData;
   })
   .then(function (moreData) {
     // use the latitude and longitude for the next fetch call
-    fetch(queryURLtwo)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=02198656acbfe2c2afe6c0d052bc513c`)
       .then(function (response) {
         return response.json()
       })
       .then(function (finalData) {
         // Here is your final data!!
-        var weather = finalData.weather.main;
-        return weather
+        var weather = finalData.weather[0].main;
+        console.log(weather);
+        return weather;
       })
   });
