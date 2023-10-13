f17db0d4dd48fd489ecf555dd86c3c7400d87514
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-13 08:50:44 -0500

    working on JS

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,7 +2,7 @@ var APIKey = '02198656acbfe2c2afe6c0d052bc513c';
 var city = 'London';
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=02198656acbfe2c2afe6c0d052bc513c")
+fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&appid=02198656acbfe2c2afe6c0d052bc513c")
   .then(function (response) {
     return response.json()
   })
@@ -10,17 +10,17 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=02198
     // here you can take the data you receive and send any part of it to the next then() block.
     const latitude = data.lat;
     const long = data.lon;
-    return latitude
-    return long
+    return { latitude, long };
   })
   .then(function (coolDataFromAbove) {
     // use the latitude and longitude for the next fetch call
-    fetch("api.openweathermap.org/data/2.5/forecast?lat=latitude&lon=long&appid=02198656acbfe2c2afe6c0d052bc513c")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${long}&appid=02198656acbfe2c2afe6c0d052bc513c")
       .then(function (response) {
         return response.json()
       })
       .then(function (finalData) {
         // Here is your final data!!
-        const weather = weather.main;
+        const weather = finalData.weather.main;
+        return weather
       })
   });
