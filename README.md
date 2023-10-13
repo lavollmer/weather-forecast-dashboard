79f8185466e4d11a32a54989e0dbe3da75bd9f8f
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-10-13 14:51:57 -0500

    updated README

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 ## project description
 
-## usage
+This project involves creating a forecast page for a specific city.
 
+This project utilizes APIs, bootstrap, dayjs.
+
+## usage
+Screenshot:
+
+Deployment Link:
 ## credits
 
+The project was done in conjunction with the U of MN bootcamp.
 ## license
 MIT license
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -18,13 +18,13 @@ fetch("https://api.openweathermap.org/geo/1.0/direct?q=London&appid=02198656acbf
   })
   .then(function (moreData) {
     // use the latitude and longitude for the next fetch call
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=02198656acbfe2c2afe6c0d052bc513c`)
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=latitude&lon=longitude&appid=02198656acbfe2c2afe6c0d052bc513c")
       .then(function (response) {
         return response.json()
       })
       .then(function (finalData) {
         // Here is your final data!!
-        var weather = finalData.weather[0].main;
+        var weather = weather[0].main;
         console.log(weather);
         return weather;
       })
